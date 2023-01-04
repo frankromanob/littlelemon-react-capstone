6271a234c71c0b163a43015d1f0d4b26138809d9
Frank Romano <romano.fr@gmail.com>
2023-01-04 15:46:59 -0400

    styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import foodimg from '../img/restauranfood.jpg'
 
 function Footer() {
 return(
-  <div>
-    <h1>Doormat Navigation</h1>
-    <ul>
-    <li><a href='#'>Home</a></li>
-      <li><a href='#'>Menu</a></li>
-      <li><a href='#'>About</a></li>
-      <li><a href='#'>Order Online</a></li>
-      <li><a href='#'>Login</a></li>
-    </ul>
+  <div class="Footer_outer">
+     <img class="foodimg" src={foodimg} alt='Little Lemon' />
+    <div class="Footer_inner_left">
+        <h2>Main</h2>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#'>About</a></li>
+        <li><a href='#'>Menu</a></li>
+        <li><a href='#'>Reservations</a></li>
+        <li><a href='#'>Order Online</a></li>
+        <li><a href='#'>Login</a></li>
+    </div>
+    <div class="Footer_inner_center">
+        <h2>Contact</h2>
+        <li><a href='mailto:contact@littlelemon.com'>Email</a></li>
+        <li><a href='tel:+18098098091'>Tel: +1(809)809-8091</a></li>
+        <li><a href='https://maps.google.com/maps?q=New+York'>Address: New York City</a></li>
+    </div>
+    <div class="Footer_inner_right">
+        <h2>Social Networks</h2>
+        <li><a href='http://instagram.com'>Instagram</a></li>
+        <li><a href='http://facebook.com'>Facebook</a></li>
+    </div>
   </div>
 )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import logoimg from '../img/Logo.svg';
 
 function Nav() {
 return(
-  <div>
-    <h1>Navigation</h1>
-    <ul>
-      <li><a href='#'>Home</a></li>
-      <li><a href='#'>Menu</a></li>
-      <li><a href='#'>About</a></li>
-      <li><a href='#'>Order Online</a></li>
-      <li><a href='#'>Login</a></li>
-    </ul>
-  </div>
+    <nav>
+        <img class="navlogo" src={logoimg} alt='Little Lemon' />
+        <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Menu</a></li>
+            <li><a href='#'>Reservations</a></li>
+            <li><a href='#'>Order Online</a></li>
+            <li><a href='#'>Login</a></li>
+        </ul>
+    </nav>
+
 )
 }
 
