bab43c2fbe1979bd0003a96cf41b675a65f94aea
Frank Romano <romano.fr@gmail.com>
2023-01-29 08:28:50 -0400

    about

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 function About() {
 return(
-  <div className='home'>
-    <h2>About Little Lemon</h2>
+  <div className='caja'>
+        <h2>About Little Lemon</h2>
     <article>
-        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h2>
+        <h2>Little Lemon, LLC is a personal chef company based in Chicago.</h2>
     </article>
+    <div className="caja-about">
+        <div className="img-green">
+          <img src="/img/Mario and Adrian A.jpg" className="card-img-2" alt="Mario and Adrian" />
+          <p>Mario and Adrian share a vision of good food and happy people</p>
+        </div>
+        <div className="img-green">
+          <img src="/img/restaurant.jpg" className="card-img-2" alt="Our restaurant" />
+          <p>Our tables are always ready for you</p>
+        </div>
+    </div>
   </div>
 )
 }
